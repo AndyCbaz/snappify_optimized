3355bad4a6c19e2384af3009a459888750362eed
Ing. Andres Bonilla <andy.cbr.tr@gmail.com>
2024-06-16 23:49:24 -0500

    carga perezosa de componentes

--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Box } from "./CustomBox";
-export { default as Button } from "./CustomButton";
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,17 @@
-import React from "react";
-import { Box, Button } from "../components";
+import React, { lazy, Suspense } from "react";
+
+const Box = lazy(() => import("../components/CustomBox"));
+const Button = lazy(() => import("../components/CustomButton"));
 
 const HomePage: React.FC = () => {
   return (
-    <Box className="h-dvh w-full border-2 border-red-500">
-      <main className="flex flex-col">
-        <Button>Click me</Button>
-      </main>
-    </Box>
+    <Suspense fallback={<div>Cargando...</div>}>
+      <Box className="h-dvh w-full border-2 border-red-500">
+        <main className="flex flex-col">
+          <Button>Click me</Button>
+        </main>
+      </Box>
+    </Suspense>
   );
 };
 
