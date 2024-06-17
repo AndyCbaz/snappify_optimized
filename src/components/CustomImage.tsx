8032e4b003b3aa5503173bc0904fb2f7e4130cfe
Ing. Andres Bonilla <andy.cbr.tr@gmail.com>
2024-06-17 02:23:04 -0500

    header mas optimizado x2

--- src/components/CustomImage.tsx
+++ src/components/CustomImage.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { Image, ImageProps } from "@chakra-ui/react";
+import { Image as ImageChakra, ImageProps } from "@chakra-ui/react";
 
 // Definimos las propiedades del CustomBox extendiendo las propiedades de Box
 interface CustomImageProps extends ImageProps {
   // Puedes agregar propiedades personalizadas aquí si es necesario
 }
 
-const CustomImage: React.FC<CustomImageProps> = ({ children, ...props }) => {
-  return <Image {...props}>{children}</Image>;
+const CustomImage: React.FC<CustomImageProps> = ({ src, ...props }) => {
+    React.useEffect(() => {
+      const img = new Image();
+      img.src = src as string;
+    }, [src]);
+  return <ImageChakra src={src} {...props}/>;
 };
 
 export default CustomImage;
