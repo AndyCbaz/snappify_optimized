e3574d52337ae35dd54bbd2c2876026e259e2125
Ing. Andres Bonilla <andy.cbr.tr@gmail.com>
2024-06-16 23:17:37 -0500

    second test performance

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    compression({
-      algorithm: "gzip",
-      ext: ".gz",
-    }),
   ],
   server: {
     host: "0.0.0.0",
